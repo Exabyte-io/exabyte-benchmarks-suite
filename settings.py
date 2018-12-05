fbb661b7b01e71783d86316a9b42fcc5080ab120
Mohammad Mohammadi <mohammad@exabyte.io>
2018-12-04 20:52:49 -0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -9,18 +9,27 @@ SITE_NAME = ""
 # Example: "East-US-zone-a"
 SITE_LOCATION = ""
 
+# Name of the files to store the benchmark runtime, CPU and Memory info respectively.
 RUNTIME_FILE = "runtime"
 CPU_INFO_FILE = "cpuinfo"
 MEM_INFO_FILE = "meminfo"
+
+# Name of the file containing the job RMS template.
 RMS_JOB_FILE_NAME = "job.rms"
-RESULTS_FILE_NAME = "results.json"
-RESULTS_FILE_PATH = os.path.join(os.path.dirname(__file__), RESULTS_FILE_NAME)
 RMS_JOB_FILE_PATH = os.path.join(os.path.dirname(__file__), RMS_JOB_FILE_NAME)
 
-PUBLISH_RESULTS = True  # whether to publish the results
+# Name of the JSON file to store the benchmark results.
+RESULTS_FILE_NAME = "results.json"
+RESULTS_FILE_PATH = os.path.join(os.path.dirname(__file__), RESULTS_FILE_NAME)
+
+# Whether to publish the results. Set it to False to disable it.
+PUBLISH_RESULTS = True
+
+# Google Cloud Function endpoint to send the benchmark results to.
 REMOTE_SOURCE_API_URL = "https://us-central1-exabyte-io.cloudfunctions.net/Exabyte-Benchmarks-Results"
 
-# Resource Management System (RMS)
+# Resource Management System (RMS) settings for PBS/Torque. Adjust settings to accommodate for other resource managers.
+# http://docs.adaptivecomputing.com/torque/6-1-0/adminGuide/help.htm#topics/torque/2-jobs/submittingManagingJobs.htm
 PPN = 36
 QUEUE = "OF"
 NOTIFY = "abe"
@@ -35,14 +44,17 @@ DEFAULT_RMS_CONFIG = {
     "EMAIL": EMAIL,
     "RUNTIME_FILE": RUNTIME_FILE,
 }
+
+# Name of qsub command
 QSUB_COMMAND = "qsub"
 
-# MODULES
+# Environment Modules Settings
 HPL_MODULE = "hpl/22-i-174-impi-044"
 VASP_MODULE = "vasp/535-i-174-impi-044"
 ESPRESSO_MODULE = "espresso/540-i-174-impi-044"
 GROMACS_MODULE = "gromacs/20183-i-174-impi-044-md"
 
+# Regular expressions to extract the HPL results (N, NB, P, Q, TIME and GFLOPS)
 REGEX = {
     'int': r'[+-]?\d+',
     'double': r'[-+]?\d*\.\d+(?:[eE][-+]?\d+)?',
