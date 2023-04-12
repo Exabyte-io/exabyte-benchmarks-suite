e2cc18eb3c281e534092af4aa1e3dd8d5f52872d
Gabriel Hare <ghare@reification.io>
2023-04-11 22:34:36 -0700

    Generalize utils read and write to handle non utf-8 data as bytes

--- benchmarks/utils.py
+++ benchmarks/utils.py
@@ -4,11 +4,28 @@ import json
 
 
 def read(path):
-    with open(path) as f:
-        return f.read()
+    with open(path, "rb") as f:
+        # PROBLEM: Not all input file formats are utf-8
+        # SOLUTION: Read bytes from file, then attempt to decode to string
+        file_bytes = f.read()
+        try:
+            file_string = file_bytes.decode("utf-8")
+            return file_string
+        except UnicodeError:
+            #print(f"Unable to decode file '{path}' - returning bytes")
+            pass
+        return file_bytes
 
 
 def write(path, content, mode="w+"):
+    # PROBLEM: Not all output file content is utf-8
+    # SOLUTION: Encode string as bytes, then write bytes to file
+    if type(content) == str:
+        content = content.encode("utf-8")
+    elif type(content) != bytes:
+        raise TypeError()
+    if "b" not in mode:
+        mode = "b" + mode
     with open(path, mode) as f:
         f.write(content)
 
